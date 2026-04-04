834325d11f3ce98753f6cf20cb21758cb48c18dc
karimhany1984 <137601144+karimhany1984@users.noreply.github.com>
2026-04-04 17:48:52 +0200

    Add template.xlsx to pre-cache assets
    Added template.xlsx to pre-cache assets for offline availability.

--- sw.js
+++ sw.js
@@ -6,7 +6,8 @@ const PRE_CACHE_ASSETS = [
     Base ,
     BASE + 'index.html',
     BASE + 'manifest.json',
-    BASE + 'icon.png'
+    BASE + 'icon.png',
+    BASE + 'template.xlsx'
 ];
 
 // Install: Only cache the essentials
