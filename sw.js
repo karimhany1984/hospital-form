00916fb0f086929794aba2ad5e586b933490968c
karimhany1984 <137601144+karimhany1984@users.noreply.github.com>
2026-06-26 15:05:49 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hospital-form-dynamic-v78';
+const CACHE_NAME = 'hospital-form-dynamic-v79';
 const BASE = '/hospital-form/';  // Match your GitHub Pages repo name
 
 // Files that MUST be available offline immediately
